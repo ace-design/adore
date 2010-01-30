355e7cb9d06fe649286b3ca0f3b274b1651d4721
sebastien.mosser <sebastien.mosser@0b8083cb-f006-4fd0-b4fa-2c990c300730>
2010-01-30 17:05:42 +0000

    oops in Picasa

--- experiences/picweb_aspectJ/picweb/Picasa.java
+++ experiences/picweb_aspectJ/picweb/Picasa.java
@@ -6,7 +6,7 @@ public class Picasa {
 
     public String[] getOnTag(String tag) {
 	Random bag = new Random();
-	String[] res = new String[bag.nextInt(20)];
+	String[] res = new String[4+bag.nextInt(20)];
 	for(int i = 0; i < res.length; i++) {
 	    res[i] = "http://picasa/" + tag + "/"+i+"/" + 
 		Math.abs(bag.nextInt()) +".jpg";
