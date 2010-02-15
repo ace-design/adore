ed9bc9b94420a26c9a7c4944233f684875aa65dd
sebastien.mosser <sebastien.mosser@0b8083cb-f006-4fd0-b4fa-2c990c300730>
2010-02-15 21:50:14 +0000

    taosd new implementation

--- case_studies/cccms/cccms_knowledge.pl
+++ case_studies/cccms/cccms_knowledge.pl
@@ -1,1 +1,18 @@
-bar(geek).
+
+shouldDraw(true).    %% Draw resulting process as PNG file
+%%shouldDraw(false). %% Do not draw resulting process as PNG file
+
+
+%%% Variable Equivalences
+adoreEquivalence(variable,[[replaceMission, mi], [askedMissionFailure, mi]]).
+
+%%%%
+%% Prolog Predicate to 'decorate' the demonstration
+%%%%
+
+cccms_draw(_,_) :- shouldDraw(false),!.
+cccms_draw(Process,PrettyName) :- 
+	swritef(RealFile,'output/%w.png',[PrettyName]),
+        adore2png(Process,RealFile).
+    
+	
