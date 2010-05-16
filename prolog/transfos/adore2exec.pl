7de84c3765338f2b4e49eac01e459410ff78a161
sebastien.mosser <sebastien.mosser@0b8083cb-f006-4fd0-b4fa-2c990c300730>
2010-05-16 19:53:07 +0000

    adore2exec sample, still a draft

--- prolog/transfos/adore2exec.pl
+++ prolog/transfos/adore2exec.pl
@@ -32,10 +32,12 @@ buildExecutionSemantic(FileName) :-
 %% Internal Definition
 %%%%
 
-%% Syntaxtic Sugar
+%%%%%%%%%%%%%%%%%%%%%%%
+%%% Syntactic Sugar %%%
+%%%%%%%%%%%%%%%%%%%%%%%
 
 genExecForAllProcess(Text) :- 
-	findall(T,(process(P),adore2exec:genExecutionSemantic(P,T)), A),
+	findall(T,(process:exists(P),adore2exec:genExecutionSemantic(P,T)), A),
 	dumpList(A,Text).
 
 genExecutionSemantic(Process, Text) :-  
@@ -44,40 +46,84 @@ genExecutionSemantic(Process, Text) :-
 	clearTags, dumpList(FormulaList, FormulaText), 
 	swritef(Text, '// %w \n%w\n',[Process, FormulaText]).
 
+
 genActFormula(Process,Result) :- 
-	%% Computation
 	activity:belongsTo(Act, Process), buildActFormula(Act, Formula),
-	%% Prettify the formula
 	findUserRoot(Act,Root), 
 	swritef(Result, 'start(%w) => %w', [Root, Formula]).
 
-%%%%
-%% Predecessors partition
-%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Formula Building %%%
+%%%%%%%%%%%%%%%%%%%%%%%%
 
-getPredsDispatch(Act, Control, Guards, Faults) :- 
+buildActFormula(Act, Formula) :-
+	flowDispatch(Act, Ctrl, Alts),
+	Formula = ''.
+
+%%%%%%%%%%%%%%%%%%%%%
+%%% Flow Dispatch %%%
+%%%%%%%%%%%%%%%%%%%%%
+
+flowDispatch(Act, Control, Alternatives) :- 
+	%% retrieving all the predecessors
 	activity:getPredecessors(Act,RawControls), 
-	findall(A,onFailure(Act,A,_), RawFaults), 
-	flatten([RawControls,RawFaults],Raws),
-	dispatch(Act, Raws, Control, Guards , Faults).
+ 	findall(A, onFailure(Act,A,_), RawFaults), 
+	flatten([RawControls,RawFaults], Raws),
+	%% retrieving the weak wait flow (part of Alternatives)
+	findall(W,weakWait(Act,W),Weaks), removeList(Weaks, Raws, WithoutWeak),
+	%% retrieving exceptional flow (part of Alternatives)
+	findall(E,( tag(fault, E, error(Fault, FaultyAct)),
+	            \+ tag(fault, Act, error(Fault, FaultyAct))), Exceptions),
+	flatten([Weaks, Exceptions], Alternatives),
+	%% Retrieving the control-flow (the rest).
+	removeList(Exceptions, WithoutWeak, Control).
 
-dispatch(_,[],[],[],[]). %% Empty List
-dispatch(Root, [H|T], Control, Guards, [[H|SimFaults]|Faults]) :- 
-	%% Fault branch pouring into normal activity flow
-	tag(fault, H, error(Fault, FaultyAct)),
-	\+ tag(fault, Root, error(Fault, FaultyAct)),
-	getSimFaults(T, error(Fault, FaultyAct), SimFaults),
-	removeList(SimFaults,T,RemovedTail),
-	dispatch(Root, RemovedTail, Control, Guards, Faults),!.
-dispatch(Root, [H|T], [H|Control], Guards, Faults) :- 
-	%% Normal continuation (aka control)
-	dispatch(Root, T, Control, Guards, Faults).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Control Partition (exclusivity induced by guards) %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-getSimFaults(List,Meta,Result) :- 
-	findall(A,(member(A,List), tag(fault,A,Meta)), Result),!.
+getConditions(Activities, Conditions) :- 
+	findall(C, (member(A,Activities), getActConditionTags(A,C)), Raws),
+	sort(Raws, WithoutDuplicates), 
+	sortByLength(WithoutDuplicates, Conditions).
+
+getActConditionTags(Act, Tags) :- 
+	findall(C, tag(guard, Act, condition(C,_,_)), Raws),
+	sort(Raws, Tags).
+
+%%%%%%%%%%%%%%%
+%%% Helpers %%%
+%%%%%%%%%%%%%%%
+
+dumpList([],'') :- !.
+dumpList([H|T], Text) :- 
+	dumpList(T,TailText), swritef(Text, "%w\n%w", [H,TailText]).
+
+sortByLength([],[]).
+sortByLength([H|T],R) :- 
+	member(L,T), length(L,Other), length(H,Mine), Mine > Other, !,
+	append(T,[H],Tmp), sortByLength(Tmp,R).
+sortByLength([H|T], [H|R]) :- sortByLength(T,R).
 
 
-%% dispatch(Root, [H|T], Control, [[[H|Sim],NotSim]|Guards], Faults) :- 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% OLD CODE, DO NOT EDIT !!!!! %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+%% %%%%
+%% %% Predecessors partition
+%% %%%%
+
+%% getPredsPartition(Act, Control, Guards, Faults) :- 
+%% 	activity:getPredecessors(Act,RawControls), 
+%% 	findall(A,onFailure(Act,A,_), RawFaults), 
+%% 	flatten([RawControls,RawFaults],Raws),
+%% 	partition(Act, Raws, Control, Guards , Faults).
+
+%% partition(_,[],[],[],[]). %% Empty List
+%% partition(Root, [H|T], Control, [[[H|Sim],NotSim]|Guards], Faults) :- 
 %% 	%% Exclusive condition detected in the Predecessors flags
 %% 	tag(guard, H ,condition(Var, Test, Val)), 
 %% 	\+ tag(guard, Root, condition(Var, Test, Val)), 
@@ -85,100 +131,95 @@ getSimFaults(List,Meta,Result) :-
 %% 	getSimGuards(T, condition(Var, Test, NotVal), NotSim),
 %% 	flatten([Sim,NotSim], ToDelete), 
 %% 	removeList(ToDelete,T,RemovedTail),
-%% 	dispatch(Root, RemovedTail, Control, Guards, Faults),!.
+%% 	partition(Root, RemovedTail, Control, Guards, Faults),!.
+%% partition(Root, [H|T], Control, Guards, [[H|SimFaults]|Faults]) :- 
+%% 	%% Fault branch pouring into normal activity flow
+%% 	tag(fault, H, error(Fault, FaultyAct)),
+%% 	\+ tag(fault, Root, error(Fault, FaultyAct)),
+%% 	getSimFaults(T, error(Fault, FaultyAct), SimFaults),
+%% 	removeList(SimFaults,T,RemovedTail),
+%% 	partition(Root, RemovedTail, Control, Guards, Faults),!.
+%% partition(Root, [H|T], [H|Control], Guards, Faults) :- 
+%% 	%% Normal continuation (aka control)
+%% 	partition(Root, T, Control, Guards, Faults).
+
+%% getSimGuards(List,Meta,Result) :- 
+%% 	findall(A,(member(A,List), tag(guard,A,Meta)),Result),!.
+
+%% getSimFaults(List,Meta,Result) :- 
+%% 	findall(A,(member(A,List), tag(fault,A,Meta)), Result),!.
+
+%% %% (Hard) Computation
+
+%% buildActFormula(Act, 'true') :- \+ path(_,Act), !. %% entry point
+%% buildActFormula(Act,Formula) :-  %% normal case
+%% 	getPredsPartition(Act, Control, Guards, Faults), 
+%% 	buildCtrlFormula(Act,Control,CtrlForm), 
+%% 	buildGuardFormula(Act,Guards,GuardForm),
+%% 	buildFaultFormula(Act,Faults,FaultForm),
+%% 	buildFinalFormula(CtrlForm,GuardForm, FaultForm, Formula),!.
+%% buildActFormula(_, 'false'). %% unknown case (should never happend)
+
+%% %%%%
+%% %% Building pretty equations (helpers)
+%% %%%%
+
+%% buildRelation(Act, Pred, R) :- 
+%% 	waitFor(Act,Pred), findUserRoot(Pred,Root), 
+%% 	swritef(R,'end(%w)',[Root]).
+%% buildRelation(Act, Pred, R) :- 
+%% 	isGuardedBy(Act, Pred, Var, Val), 
+%% 	findUserRoot(Pred,PredRoot), findUserRoot(Var,VarRoot),
+%% 	swritef(R,'(end(%w) & %w(%w))',[PredRoot,Val,VarRoot]).
+%% buildRelation(Act, Pred, R) :- 
+%% 	onFailure(Act, Pred, Fault), findUserRoot(Pred,Root),
+%% 	swritef(R,'fail(%w,%w)',[Root,Fault]).
 
 
-	
-%% extractConditions([],[]).
-%% extractConditions([H|Tail], [Conds|Others]) :- 
-%% 	findall(Var,tag(guard,H,condition(Var,_,_)),Raw), sort(Raw,Conds),
-%% 	extractConditions(Tail,Others).
+%% buildCtrlFormula(_,[],'').
+%% buildCtrlFormula(Root,L,Result) :- 
+%% 	mapOperation('&', Root, L, Result).
 
-%% partition([],_,[]).
-%% partition(L,[],L).
-%% partition(Acts,[[Cond]|Tail],L) :- 
-%% 	partitionOncondition(Cond, Acts, Done, Rest),
-%% 	removeConditionInList
+%% buildGuardFormula(_,[],'').
+%% buildGuardFormula(Root,[[OnValList,OnNotValList]],Result) :-
+%% 	mapOperation('&', Root, OnValList, OnValForm),
+%% 	mapOperation('&', Root, OnNotValList, OnNotValForm),
+%% 	swritef(Result,'((%w) | (%w))',[OnValForm,OnNotValForm]), !.
+%% buildGuardFormula(Root,[[OnValList,OnNotValList]|Tail],Result) :-
+%% 	mapOperation('&', Root, OnValList, OnValForm),
+%% 	mapOperation('&', Root, OnNotValList, OnNotValForm),
+%% 	buildGuardFormula(Root,Tail,Others),
+%% 	swritef(Result,'((%w) | (%w)) & %w',[OnValForm,OnNotValForm,Others]).
 
-
-%% (Hard) Computation
-
-buildActFormula(Act, 'true') :- \+ path(_,Act), !. %% entry point
-buildActFormula(Act,Formula) :-  %% normal case
-	getPredsDispatch(Act, Control, Guards, Faults), 
-	buildCtrlFormula(Act,Control,CtrlForm), 
-	buildGuardFormula(Act,Guards,GuardForm),
-	buildFaultFormula(Act,Faults,FaultForm),
-	buildFinalFormula(CtrlForm,GuardForm, FaultForm, Formula),!.
-buildActFormula(_, 'false'). %% unknown case (should never happend)
-
-%%%%
-%% Building pretty equations (helpers)
-%%%%
-
-buildRelation(Act, Pred, R) :- 
-	waitFor(Act,Pred), findUserRoot(Pred,Root), 
-	swritef(R,'end(%w)',[Root]).
-buildRelation(Act, Pred, R) :- 
-	isGuardedBy(Act, Pred, Var, Val), findUserRoot(Pred,PredRoot),
-	( tag(guard, Act, condition(Var, Pred, Val)), findUserRoot(Var,VarRoot),
-	  swritef(R,'(end(%w) & %w(%w))',[PredRoot,Val,VarRoot])
-	| \+ tag(guard, Act, condition(Var, Pred, Val)), 
-	  swritef(R,'end(%w)',[PredRoot])).
-buildRelation(Act, Pred, R) :- 
-	onFailure(Act, Pred, Fault), findUserRoot(Pred,Root),
-	swritef(R,'fail(%w,%w)',[Root,Fault]).
+%% buildFaultFormula(_,[],'').
+%% buildFaultFormula(Root,[List],Result) :- 
+%% 	mapOperation('&',Root,List,Result), !.
+%% buildFaultFormula(Root,[List|Tail],Result) :- 
+%% 	mapOperation('&',Root,List, This),
+%% 	buildFaultFormula(Root,Tail,Others), 
+%% 	swritef(Result,'%w & %w',[This, Others]).
 
 
-buildCtrlFormula(_,[],'').
-buildCtrlFormula(Root,L,Result) :- 
-	mapOperation('&', Root, L, Result).
-
-buildGuardFormula(_,[],'').
-buildGuardFormula(Root,[[OnValList,OnNotValList]],Result) :-
-	mapOperation('&', Root, OnValList, OnValForm),
-	mapOperation('&', Root, OnNotValList, OnNotValForm),
-	swritef(Result,'((%w) | (%w))',[OnValForm,OnNotValForm]), !.
-buildGuardFormula(Root,[[OnValList,OnNotValList]|Tail],Result) :-
-	mapOperation('&', Root, OnValList, OnValForm),
-	mapOperation('&', Root, OnNotValList, OnNotValForm),
-	buildGuardFormula(Root,Tail,Others),
-	swritef(Result,'((%w) | (%w)) & %w',[OnValForm,OnNotValForm,Others]).
-
-buildFaultFormula(_,[],'').
-buildFaultFormula(Root,[List],Result) :- 
-	mapOperation('&',Root,List,Result), !.
-buildFaultFormula(Root,[List|Tail],Result) :- 
-	mapOperation('&',Root,List, This),
-	buildFaultFormula(Root,Tail,Others), 
-	swritef(Result,'%w & %w',[This, Others]).
-
-
-buildFinalFormula(Control, '', '', Control) :- !.
-buildFinalFormula('', Guards, '', Guards) :- !.
-buildFinalFormula('', '', Faults, Faults) :- !.
-buildFinalFormula(Control, Guards, '', Result) :- 
-	swritef(Result,'%w & %w',[Control, Guards]), !.
-buildFinalFormula(Control, '', Faults, Result) :- 
-	swritef(Result,'(%w) | (%w)',[Control, Faults]), !.
-buildFinalFormula(Control, Guards, Faults, Result) :- 
-	swritef(Result,'(%w & %w) | (%w)',[Control, Guards, Faults]).
+%% buildFinalFormula(Control, '', '', Control) :- !.
+%% buildFinalFormula(Control, Guards, '', Result) :- 
+%% 	swritef(Result,'%w & %w',[Control, Guards]), !.
+%% buildFinalFormula(Control, '', Faults, Result) :- 
+%% 	swritef(Result,'(%w) | (%w)',[Control, Faults]), !.
+%% buildFinalFormula(Control, Guards, Faults, Result) :- 
+%% 	swritef(Result,'(%w & %w) | (%w)',[Control, Guards, Faults]).
 	
 
-%%%%
-%% Helper:
-%%%%	
+%% %%%%
+%% %% Helper:
+%% %%%%	
 
-mapOperation(_,_,[],'').
-mapOperation(_, Root, [Act], Result) :- 
-	buildRelation(Root,Act,Result), !.
-mapOperation(Op, Root, [Act|Tail], Result) :- 
-	buildRelation(Root,Act, This), mapOperation(Op, Root, Tail, Others),
-	swritef(Result,'%w %w %w',[This, Op, Others]), !.
+%% mapOperation(_,_,[],'').
+%% mapOperation(_, Root, [Act], Result) :- 
+%% 	buildRelation(Root,Act,Result), !.
+%% mapOperation(Op, Root, [Act|Tail], Result) :- 
+%% 	buildRelation(Root,Act, This), mapOperation(Op, Root, Tail, Others),
+%% 	swritef(Result,'%w %w %w',[This, Op, Others]), !.
 
-invert(true,false).
-invert(false,true).
+%% invert(true,false).
+%% invert(false,true).
 
-dumpList([],'') :- !.
-dumpList([H|T], Text) :- 
-	dumpList(T,TailText), swritef(Text, "%w\n%w", [H,TailText]).
